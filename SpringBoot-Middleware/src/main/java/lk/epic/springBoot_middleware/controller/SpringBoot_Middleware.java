3cba05934ef2fd293e1138341541c91b9fc61f64
AshanSandeep06 <ashansandeep06@gmail.com>
2023-07-09 12:39:56 +0530

    Spring-Boot Middleware has been completed

--- SpringBoot-Middleware/src/main/java/lk/epic/springBoot_middleware/controller/SpringBoot_Middleware.java
+++ SpringBoot-Middleware/src/main/java/lk/epic/springBoot_middleware/controller/SpringBoot_Middleware.java
@@ -2,7 +2,10 @@ package lk.epic.springBoot_middleware.controller;
 
 import lk.epic.springBoot_middleware.ISO8583Message.ISO8583Message;
 import lk.epic.springBoot_middleware.dto.ISO8583FieldsDTO;
+import lk.epic.springBoot_middleware.entity.ISO8583Fields;
+import lk.epic.springBoot_middleware.util.ResponseUtil;
 import org.jpos.iso.ISOMsg;
+import org.jpos.iso.ISOUtil;
 import org.jpos.iso.packager.ISO87APackager;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,13 +24,15 @@ public class SpringBoot_Middleware {
     private static DataOutputStream dataOutputStream;
 
     @PostMapping
-    public void workWithJsonAndISO8583(@RequestBody ISO8583FieldsDTO allFields) {
+    public ResponseUtil workWithJsonAndISO8583(@RequestBody ISO8583FieldsDTO allFields) {
         try {
             System.out.println(allFields);
 
             remoteSocket = new Socket("localhost", PORT);
             dataInputStream = new DataInputStream(remoteSocket.getInputStream());
             dataOutputStream = new DataOutputStream(remoteSocket.getOutputStream());
+
+            ISO87APackager packager = new ISO87APackager();
 
             ISOMsg message = ISO8583Message.getInstance().getIsoMessage();
             message.setMTI(allFields.getF0());
@@ -44,12 +49,42 @@ public class SpringBoot_Middleware {
             message.set(55, allFields.getF55());
             message.set(62, allFields.getF62());
 
-            message.setPackager(new ISO87APackager());
+            message.setPackager(packager);
             byte[] packedData = message.pack();
+
+            dataOutputStream.writeInt(packedData.length);
+            dataOutputStream.write(packedData);
+            dataOutputStream.flush();
+
+            int responseLength = dataInputStream.readInt();
+            byte[] responseData = new byte[responseLength];
+            dataInputStream.readFully(responseData);
+
+            //Unpack the response message
+            ISOMsg iso8583Response = ISO8583Message.getInstance().getIsoMessage();
+            iso8583Response.setPackager(packager);
+            iso8583Response.unpack(responseData);
+
+            ISO8583Fields iso8583Entity = new ISO8583Fields();
+
+            iso8583Entity.setF0(iso8583Response.getString(0));
+            iso8583Entity.setF3(iso8583Response.getString(3));
+            iso8583Entity.setF4(iso8583Response.getString(4));
+            iso8583Entity.setF11(iso8583Response.getString(11));
+            iso8583Entity.setF22(iso8583Response.getString(22));
+            iso8583Entity.setF24(iso8583Response.getString(24));
+            iso8583Entity.setF25(iso8583Response.getString(25));
+            iso8583Entity.setF35(iso8583Response.getString(35));
+            iso8583Entity.setF41(iso8583Response.getString(41));
+            iso8583Entity.setF42(iso8583Response.getString(42));
+            iso8583Entity.setF55(iso8583Response.getString(55));
+            iso8583Entity.setF62(iso8583Response.getString(62));
+
+            return new ResponseUtil("OK", ISOUtil.hexString(iso8583Response.pack()), iso8583Entity);
 
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
-
     }
 }
